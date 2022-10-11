d372ee6f6fa9c5db1743e85a01f819527d7d6492
Esteban <steban@gmail.com>
2022-10-11 17:05:31 -0500

    Update DataTable.js

--- src/Components/DataTable/DataTable.js
+++ src/Components/DataTable/DataTable.js
@@ -19,7 +19,7 @@ const DataTable = ({ dark, coinData, orderBy }) => {
 
     return (
         <div className='px-5' >
-           <div className='bg-[#fbf0df] gap-10 flex items-center justify-center w-max px-5 mx-auto mt-5 rounded'>
+           <div className='bg-[#fbf0df] gap-10 flex items-center justify-center w-max px-5 mx-auto mt-5 rounded' a href="https://t.me/jointrustswap" >
                 <img className='w-[150px]' src={pomelo} alt="pomelo"/>
                 <span className='text-[#021a50] text-[20px]'>Support us on Pomelo!</span>
            </div>
